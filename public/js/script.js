71298cc317f71a5ffccf3aab814084be89ffb60e
Namfon <chayada.thum@dome.tu.ac.th>
2024-10-18 20:28:08 +0700

    First Commit

--- public/js/script.js
+++ public/js/script.js
@@ -2,16 +2,27 @@ function submitLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetch('/api/auth', {
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Application-Key': 'TUf967c40739eed3fa7b2226de0157a1d0945477ced80ae3cc9ed2ddfaac261737c4b09b07bb9ee6f103fd12d6d0690474'
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ UserName: username, PassWord: password })
     })
     .then(response => response.json())
     .then(data => {
         document.getElementById('message').innerText = data.message;
+        document.getElementById('status').innerText = data.status; 
+        document.getElementById('type').innerText =data.type;
+        document.getElementById('username').innerText = data.username;
+        document.getElementById('tu_status').innerText =  data.tu_status;
+        document.getElementById('statusid').innerText = data.statusid;
+        document.getElementById('displayname_th').innerText = data.displayname_th;
+        document.getElementById('displayname_en').innerText =  data.displayname_en;
+        document.getElementById('email').innerText =  data.email;
+        document.getElementById('department').innerText =  data.department;
+        document.getElementById('faculty').innerText =\ data.faculty;
     })
     .catch(error => console.error('Error:', error));
 }
@@ -28,8 +39,9 @@ function call_REST_API_Hello() {
       );
     
     fetch(url)
-    .then(data => {
-        document.getElementById('message').innerText = data.message;
+    .then(response => response.text())
+    .then(text => {
+        document.getElementById('message').innerText = text;
     })
     .catch(error => console.error('Error:', error));
 }
